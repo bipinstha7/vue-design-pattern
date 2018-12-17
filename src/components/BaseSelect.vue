298b2ace6865c897c12bce5a1b13f951a601827e
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-17 20:10:29 +0545

    BaseSelect component with select form options

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <label v-if="label">{{ label }}</label>
+        <select :value="value" @input="updateValue">
+            <option v-for="option in options" :value="option" :key="option" :selected="option === value">{{
+                option
+            }}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            default: ''
+        },
+        options: {
+            type: Array,
+            required: true
+        },
+        value: [String, Number]
+    },
+    methods: {
+        updateValue(event) {
+            this.$emit('input', event.target.value)
+        }
+    }
+}
+</script>
