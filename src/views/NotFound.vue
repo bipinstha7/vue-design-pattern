34dd5a59323cc629cc2dcb7695ab5075e1ab552f
bipinstha7 <bipin.shrestha901@gmail.com>
2018-12-17 20:10:29 +0545

    change resource type from Number to either Number or String- as it comes from the query string

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,17 +2,16 @@
     <div>
         <h1>Oops !</h1>
         <h3>The {{ resource }} you're looking for is not here.</h3>
-        <router-link :to="{name: 'event-list'}">Back to the home page</router-link>
+        <router-link :to="{ name: 'event-list' }">Back to the home page</router-link>
     </div>
 </template>
 <script>
 export default {
     props: {
         resource: {
-            type: Number,
+            type: [Number, String],
             required: true
         }
     }
 }
 </script>
-
