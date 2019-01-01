24388a542fad9d20cf033cdcf9b3c589e79e245d
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-01 20:45:32 +0545

    use slot to pass/show the attendees.length data

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -3,6 +3,7 @@
         <svg class="icon" :width="width" :height="height">
             <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }"></use>
         </svg>
+        <slot></slot>
     </div>
 </template>
 
