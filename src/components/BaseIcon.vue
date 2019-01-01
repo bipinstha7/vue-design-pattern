7924a0090040898e70537e7facd9023fc74152fa
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-01 18:32:40 +0545

    global base icon component

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -1,9 +1,44 @@
 <template>
-    <div>BaseIcon Component</div>
+    <div class="icon-wrapper">
+        <svg class="icon" :width="width" :height="height">
+            <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }"></use>
+        </svg>
+    </div>
 </template>
 
 <script>
-export default {}
+export default {
+    props: {
+        name: {
+            type: String,
+            default: ''
+        },
+        width: {
+            type: [Number, String],
+            default: 24
+        },
+        height: {
+            type: [Number, String],
+            default: 24
+        }
+    }
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.icon-wrapper {
+    display: inline-flex;
+    align-items: center;
+    color: rgba(0, 0, 0, 0.4);
+    font-size: 1rem;
+    font-weight: 600;
+}
+.icon {
+    stroke: currentColor;
+    stroke-width: 2;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    fill: none;
+    margin-right: 6px;
+}
+</style>
