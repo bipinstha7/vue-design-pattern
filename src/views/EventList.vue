0afcc9a2279ae973eb5311d3f96c89f79bf1105b
bipinstha7 <bipin.shrestha901@gmail.com>
2019-01-10 10:43:06 +0545

    fix event error on EventList after event store moduleratization

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <h1>Event Listing</h1>
-        <EventCard v-for="event in events" :key="event.id" :event="event" />
+        <h1>Events for {{user.user.name}}</h1>
+        <EventCard v-for="event in event.events" :key="event.id" :event="event" />
         <template v-if="page !== 1">
             <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev"
                 >Prev Page</router-link
@@ -31,7 +31,7 @@ export default {
         page() {
             return parseInt(this.$route.query.page) || 1
         },
-        ...mapState(['events'])
+        ...mapState(['event', 'user'])
     }
 }
 </script>
