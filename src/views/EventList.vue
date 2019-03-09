853c72be49c1b01e98f82665732d126dc05db867
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-09 22:27:40 +0545

    fetch events from the server and pass to EventCard as props

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,15 +1,30 @@
 <template>
     <div>
         <h1>Event Listing</h1>
-        <EventCard />
+        <EventCard v-for="event in events" :key="event.id" :event="event" />
     </div>
 </template>
 <script>
 import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
+
 export default {
     name: 'EventList',
     components: {
         EventCard
+    },
+    data() {
+        return {
+            events: []
+        }
+    },
+    created() {
+        axios
+            .get('https://raw.githubusercontent.com/bipinstha7/vue-design-pattern/master/db.json')
+            .then(response => {
+                this.events = response.data.events
+            })
+            .catch(err => console.log('err', err))
     }
 }
 </script>
