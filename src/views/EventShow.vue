cbf3ba9b698560d7e42b0d01b206993c075ea9a7
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-10 12:26:07 +0545

    show param id value to the Event Show page

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>Showing event #1</h1>
+    <h1>Showing event #{{ id }}</h1>
 </template>
+<script>
+export default {
+    props: {
+        id: {
+            type: Number,
+            default: 0
+        }
+    }
+}
+</script>
