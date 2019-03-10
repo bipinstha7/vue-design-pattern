aa13d8c53a95215583ccbcbbea74e69b293e3979
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-10 11:56:46 +0545

    router for event files

--- src/router.js
+++ src/router.js
@@ -1,24 +1,36 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import EventCreate from './views/EventCreate.vue'
+import EventList from './views/EventList.vue'
+import EventShow from './views/EventShow.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
-});
+    routes: [
+        {
+            path: '/',
+            name: 'event-list',
+            component: EventList
+        },
+        // {
+        //   path: '/about',
+        //   name: 'about',
+        //   // route level code-splitting
+        //   // this generates a separate chunk (about.[hash].js) for this route
+        //   // which is lazy-loaded when the route is visited.
+        //   component: () =>
+        //     import(/* webpackChunkName: "about" */ './views/About.vue')
+        // }
+        {
+            path: '/event',
+            name: 'event-show',
+            component: EventShow
+        },
+        {
+            path: '/event/creaet',
+            name: 'event-create',
+            component: EventCreate
+        }
+    ]
+})
