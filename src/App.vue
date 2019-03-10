28f2819cce6ff008661bcc6eda05f049bc62fb74
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-10 12:25:13 +0545

    fix event-create typo and move event-show router link to event-list

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
     <div id="app">
         <div id="nav">
             <router-link :to="{ name: 'event-list' }">List</router-link>|
-            <router-link :to="{ name: 'event-show' }">Show Event #1</router-link>
-            <router-link :to="{ name: 'event-crPeate' }">Create</router-link>
+            <router-link :to="{ name: 'event-create' }">Create</router-link>
         </div>
         <router-view />
     </div>
