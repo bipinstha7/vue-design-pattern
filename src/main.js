fb40196cec305f7851f7b4eed058c10040bde06c
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-10 13:13:43 +0545

    create BaseIcon component and declare it as a global component in main.js and use it in EventList Component

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import BaseIcon from '@/components/BaseIcon.vue'
 
-Vue.config.productionTip = false;
+Vue.component('BaseIcon', BaseIcon)
+
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,6 +2,7 @@
     <div>
         <h1>Event Listing</h1>
         <EventCard />
+        <BaseIcon />
     </div>
 </template>
 <script>
