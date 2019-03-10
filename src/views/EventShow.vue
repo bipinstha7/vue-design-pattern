b634b56ec2636c318ae2704578a1735ac1d7aed8
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-10 21:03:00 +0545

    Single event component

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,7 +1,35 @@
 <template>
-    <h1>Showing event #{{ id }}</h1>
+    <div>
+        <div class="event-header">
+            <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+            <h1 class="title">{{ event.title }}</h1>
+            <h5>Organized by {{ event.organizer }}</h5>
+            <h5>Category: {{ event.category }}</h5>
+        </div>
+
+        <BaseIcon name="map">
+            <h2>Location</h2>
+        </BaseIcon>
+
+        <address>{{ event.location }}</address>
+
+        <h2>Event details</h2>
+        <p>{{ event.description }}</p>
+
+        <h2>
+            Attendees
+            <span class="badge -fill-gradient">{{ event.attendees ? event.attendees.length : 0 }}</span>
+        </h2>
+        <ul class="list-group">
+            <li v-for="(attendee, index) in event.attendees" :key="index" class="list-item">
+                <b>{{ attendee.name }}</b>
+            </li>
+        </ul>
+    </div>
 </template>
 <script>
+import EventService from '@/services/EventService.js'
+
 export default {
     name: 'EventShow',
     props: {
@@ -9,6 +37,38 @@ export default {
             type: Number,
             default: 0
         }
+    },
+    data() {
+        return {
+            event: {}
+        }
+    },
+    created() {
+        EventService.getEvent(this.id)
+            .then(response => {
+                this.event = response.data
+            })
+            .catch(err => console.log('err', err))
     }
 }
 </script>
+<style scoped>
+.location {
+    margin-bottom: 0;
+}
+.location > .icon {
+    margin-left: 10px;
+}
+.event-header > .title {
+    margin: 0;
+}
+.list-group {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+.list-group > .list-item {
+    padding: 1em 0;
+    border-bottom: solid 1px #e5e5e5;
+}
+</style>
