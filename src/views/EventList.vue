ceace772626a345498346d724da0c7aac635fe75
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-11 22:29:49 +0545

    get fetched data from the vuex and show in the page - EventList views component with pagination

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,28 +2,36 @@
     <div>
         <h1>Event Listing</h1>
         <EventCard v-for="event in events" :key="event.id" :event="event" />
+        <template v-if="page !== 1">
+            <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev"
+                >Prev Page</router-link
+            >|
+        </template>
+        <router-link :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next"
+            >Next Page</router-link
+        >
     </div>
 </template>
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import { mapState } from 'vuex'
 
 export default {
     name: 'EventList',
     components: {
         EventCard
     },
-    data() {
-        return {
-            events: []
-        }
-    },
     created() {
-        EventService.getEvents()
-            .then(response => {
-                this.events = response.data
-            })
-            .catch(err => console.log('err', err))
+        this.$store.dispatch('fetchEvents', {
+            perPage: 2,
+            page: this.page
+        })
+    },
+    computed: {
+        page() {
+            return parseInt(this.$route.query.page) || 1
+        },
+        ...mapState(['events'])
     }
 }
 </script>
