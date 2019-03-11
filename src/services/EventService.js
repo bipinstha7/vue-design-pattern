9ac6d8e1f003437137dc19fdebb4f2408ecb4609
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-11 22:28:34 +0545

    send parameters to getEvents for pagination

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/bipinstha7/vue-design-pattern',
+    // baseURL: 'https://my-json-server.typicode.com/bipinstha7/vue-design-pattern',
+    baseURL: 'http://localhost:3000',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
@@ -9,8 +10,9 @@ const apiClient = axios.create({
 })
 
 export default {
-    getEvents() {
-        return apiClient.get('/events')
+    getEvents(perPage, page) {
+        console.log('page', page, 'limit', perPage)
+        return apiClient.get(`/events?_page=${page}&_limit=${perPage}`)
     },
     getEvent(id) {
         return apiClient.get(`/events/${id}`)
