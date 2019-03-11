130d9c407bf2ae0dc09779e6f8e39ab4f8c17885
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-11 21:40:17 +0545

    create createEvent service to create new events

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -14,5 +14,8 @@ export default {
     },
     getEvent(id) {
         return apiClient.get(`/events/${id}`)
+    },
+    postEvent(event) {
+        return apiClient.post('/events', event)
     }
 }
