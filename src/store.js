f900b9fb67a633aeea6fd900e3aac17e74ef2bc2
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-11 21:39:24 +0545

    state, mutations and event in store to create event

--- src/store.js
+++ src/store.js
@@ -1,10 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {}
+    state: {
+        user: { id: '111aaa', name: 'Jamie Lannister' },
+        categories: [
+            'sustainability',
+            'nature',
+            'animal welfare',
+            'housing',
+            'education',
+            'food',
+            'community'
+        ],
+        events: []
+    },
+    mutations: {
+        ADD_EVENT(state, event) {
+            state.events.push(event)
+        }
+    },
+    actions: {
+        /* update to store only after successful data store */
+        createEvent({ commit }, event) {
+            return EventService.postEvent(event).then(() => {
+                commit('ADD_EVENT', event)
+            })
+        }
+    }
 })
