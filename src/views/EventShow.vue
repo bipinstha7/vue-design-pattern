8f5fd0e312dfc542c45152ce3ad4a1dc3d7e5f15
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-12 13:47:26 +0545

    implement NProgress start and done in beforeRouteEnter

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -28,7 +28,9 @@
     </div>
 </template>
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState } from 'vuex'
+import NProgress from 'nprogress'
+import store from '@/store/store'
 
 export default {
     name: 'EventShow',
@@ -38,13 +40,18 @@ export default {
             default: 0
         }
     },
-    created() {
-        this.fetchEvent(this.id)
+    beforeRouteEnter(to, from , next) {
+        NProgress.start()
+
+        store.dispatch('event/fetchEvent', to.params.id)
+            .then(() => {
+                NProgress.done()
+                next()
+            })
     },
     computed: mapState({
         event: state => state.event.event
     }),
-    methods: mapActions('event', ['fetchEvent'])
 }
 </script>
 <style scoped>
