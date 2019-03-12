69d2ea70595bcf9372794724770d7ff8234aff2c
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-12 11:39:41 +0545

    dispatch notification action when the error occurs and the new event is created

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -21,17 +21,46 @@ export const mutations = {
 
 export const actions = {
     /* update to store only after successful data store */
-    createEvent({ commit }, event) {
-        return EventService.postEvent(event).then(() => {
-            commit('ADD_EVENT', event)
-        })
+    createEvent({ commit, dispatch }, event) {
+        return EventService.postEvent(event)
+            .then(() => {
+                commit('ADD_EVENT', event)
+
+                const notification = {
+                    type: 'success',
+                    message: `Your event has been created!`
+                }
+
+                /* The way we dispatch the action of another namespaced module */
+                dispatch('notification/add', notification, {root: true})
+            })
+            .catch(err => {
+                const notification = {
+                    type: 'error',
+                    message: `There was a problem creating your event: ${err.message}`
+                }
+
+                /* The way we dispatch the action of another namespaced module */
+                dispatch('notification/add', notification, {root: true})
+
+                /* throw err so that the event create component can act upon the error, o.e stay on the same page without clearing the form */
+                throw err
+            })
     },
-    fetchEvents({ commit }, { perPage, page }) {
+    fetchEvents({ commit, dispatch }, { perPage, page }) {
         EventService.getEvents(perPage, page)
             .then(response => {
                 commit('SET_EVENTS', response.data)
             })
-            .catch(err => console.log('err', err))
+            .catch(err => {
+                const notification = {
+                    type: 'error',
+                    message: `There was a problem fetching events: ${err.message}`
+                }
+
+                /* The way we dispatch the action of another namespaced module */
+                dispatch('notification/add', notification, {root: true})
+            })
     },
     fetchEvent({ commit, getters }, id) {
         let event = getters.getEventById(id)
@@ -44,7 +73,15 @@ export const actions = {
             .then(response => {
                 commit('SET_EVENT', response.data)
             })
-            .catch(err => console.log('err', err))
+            .catch(err => {
+                const notification = {
+                    type: 'error',
+                    message: `There was a problem fetching event: ${err.message}`
+                }
+
+                /* The way we dispatch the action of another namespaced module */
+                dispatch('notification/add', notification, {root: true})
+            })
     }
 }
 
