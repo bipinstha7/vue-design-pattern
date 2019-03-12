15d0b9c8dc71024023a8369dbaca842788afd7f1
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-12 12:50:52 +0545

    NotificationBar component to show notification to user

---
+++ src/components/NotificationBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="notification-bar" :class="notificationTypeClass">
+        <p>{{notification.message}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        notification: {
+            type: Object,
+            required: true
+        }
+},
+    computed: {
+        notificationTypeClass() {
+            return `-text-${this.notification.type}`
+        }
+    }
+}
+</script>
+
+<style>
+.notification-bar {
+    margin: 1em 0 1em;
+}
+</style>
