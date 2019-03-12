8ff7f35e7c7085efbe8ae9d0900f4ac19267e4d2
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-12 13:13:23 +0545

    implement NProgress on axios interceptor as: start when requesting and stop after the response

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
 
 const apiClient = axios.create({
     // baseURL: 'https://my-json-server.typicode.com/bipinstha7/vue-design-pattern',
@@ -7,6 +8,16 @@ const apiClient = axios.create({
         Accept: 'application/json',
         'Content-Type': 'application/json'
     }
+})
+
+apiClient.interceptors.request.use(config => {
+    NProgress.start()
+    return config
+})
+
+apiClient.interceptors.response.use(response => {
+    NProgress.done()
+    return response
 })
 
 export default {
