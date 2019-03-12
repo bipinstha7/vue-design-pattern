600cb34c71527d6946f4bfeaf4822dc5a7acfec2
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-12 11:40:03 +0545

    import notification store module in main store.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,15 @@ import Vuex from 'vuex'
 
 import * as user from '@/store/modules/user.js'
 import * as event from '@/store/modules/event.js'
+import * as notification from '@/store/modules/notification.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
         user,
-        event
+        event,
+        notification
     },
     state: {
         categories: [
