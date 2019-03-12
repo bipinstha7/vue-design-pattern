dc887c26c5dc3dd4c03dab0ad6177f9d2f00a641
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-12 14:05:27 +0545

    implement NProgress start and done using Global and Per-Route Guards

--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ import Router from 'vue-router'
 import EventCreate from './views/EventCreate.vue'
 import EventList from './views/EventList.vue'
 import EventShow from './views/EventShow.vue'
+import NProgress from 'nprogress'
+import store from '@/store/store'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     mode: 'history',
     routes: [
         {
@@ -27,7 +29,11 @@ export default new Router({
             path: '/event/:id',
             name: 'event-show',
             component: EventShow,
-            props: true
+            props: true,
+            beforeEnter(to, from, next) {
+                store.dispatch('event/fetchEvent', to.params.id)
+                    .then(() => next())
+            }
         },
         {
             path: '/event/create',
@@ -36,3 +42,14 @@ export default new Router({
         }
     ]
 })
+
+router.beforeEach((to, from, next) => {
+    NProgress.start()
+    next()
+}) 
+
+router.afterEach((to, from, next) => {
+    NProgress.done()
+}) 
+
+export default router
