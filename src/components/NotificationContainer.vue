18527f0c9d8bac0db696ca4755d3e576cc5f72bf
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-13 21:50:40 +0545

    wrap NotificationBar component inside NotificationContainer Component

--- src/components/NotificationContainer.vue
+++ src/components/NotificationContainer.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="notification-container">
-        <NotificationBar 
+        <NotificationBar
             v-for="notification in notifications"
             :key="notification.id"
-            :notification= "notification"
+            :notification="notification"
         />
     </div>
 </template>
 
 <script>
 import NotificationBar from '@/components/NotificationBar.vue'
-import {mapState} from 'vuex'
+import { mapState } from 'vuex'
 
 export default {
     components: {
