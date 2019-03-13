b3ab5fc93a81272cd7e099076a04ab8ab1416480
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-13 21:50:09 +0545

    show notification with NotificationBar Component

--- src/components/NotificationBar.vue
+++ src/components/NotificationBar.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="notification-bar" :class="notificationTypeClass">
-        <p>{{notification.message}}</p>
+        <p>{{ notification.message }}</p>
     </div>
 </template>
 
 <script>
-import {mapActions} from 'vuex'
-import { setTimeout, clearTimeout } from 'timers';
+import { mapActions } from 'vuex'
+import { setTimeout, clearTimeout } from 'timers'
 
 export default {
     data() {
