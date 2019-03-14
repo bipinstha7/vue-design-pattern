1af42df7dd66c0ea5b5ecd479bbde132f49ed078
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-14 19:51:15 +0545

    add  and  in BaseSelect component

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <label v-if="label">{{ label }}</label>
-        <select :value="value" @input="updateValue">
+        <select :value="value" @input="updateValue" v-bind="$attrs" v-on="$listeners">
             <option v-for="option in options" :value="option" :key="option" :selected="option === value">{{
                 option
             }}</option>
