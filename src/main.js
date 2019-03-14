9ee24dc8b01a19c7d48456290e8981914c96927a
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-14 19:51:05 +0545

    import and use vuelidate in main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
 import 'nprogress/nprogress.css'
+import Vuelidate from 'vuelidate'
 
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+
+Vue.use(Vuelidate)
+
 const requireComponent = require.context(
     // The relative path of the components folder
     './components',
