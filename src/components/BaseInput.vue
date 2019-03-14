17ca87493203d00616686c9928029eefb5f33b0c
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-14 19:52:05 +0545

    add  and  in BaseInput component

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,25 +1,37 @@
 <template>
     <div>
         <label v-if="label">{{ label }}</label>
-        <input :type="type" :placeholder="placeholder" :value="value" @input="updateValue" />
+        <!-- Both methods gives result -->
+        <!-- <input :type="type" :placeholder="placeholder" :value="value" @input="updateValue" v-on="listeners" /> -->
+        <input :value="value" @input="updateValue" v-bind="$attrs" v-on="listeners" />
     </div>
 </template>
 
 <script>
 export default {
+    inheritAttrs: false,
     props: {
         label: {
             type: String,
             default: ''
         },
-        value: [String, Number],
-        type: {
-            type: String,
-            default: 'text'
-        },
-        placeholder: {
-            type: String,
-            default: ''
+        value: [String, Number]
+        /* We don't need these two if $attrs are used */
+        // type: {
+        //     type: String,
+        //     default: 'text'
+        // },
+        // placeholder: {
+        //     type: String,
+        //     default: ''
+        // }
+    },
+    computed: {
+        listeners() {
+            return {
+                ...this.$listeners,
+                input: this.updateValue
+            }
         }
     },
     methods: {
