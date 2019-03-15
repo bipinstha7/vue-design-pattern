30d0ac055838376545f2a0184460cde53af1367f
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-15 20:48:26 +0545

    add space between Navbar List and | (pipe) symbol

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
     <div id="nav" class="nav">
         <router-link to="/" class="brand">Real World Events</router-link>
         <nav>
-            <router-link :to="{ name: 'event-list' }">List</router-link>|
+            <router-link :to="{ name: 'event-list' }">List</router-link> |
             <router-link :to="{ name: 'event-create' }">Create</router-link>
         </nav>
     </div>
