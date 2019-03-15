07e5cf583aee6317d1c7731b6e8e3e002c81e1db
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-15 18:11:49 +0545

    base input component to hold input form component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <label v-if="label">{{ label }}</label>
+        <input :type="type" :placeholder="placeholder" :value="value" @input="updateValue" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            default: ''
+        },
+        value: [String, Number],
+        type: {
+            type: String,
+            default: 'text'
+        },
+        placeholder: {
+            type: String,
+            default: ''
+        }
+    },
+    methods: {
+        updateValue(event) {
+            this.$emit('input', event.target.value)
+        }
+    }
+}
+</script>
+
+<style></style>
